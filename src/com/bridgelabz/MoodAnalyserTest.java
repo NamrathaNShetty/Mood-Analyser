a73904206f2bad5e85b825f2fa701a4ea5f81014
NamrathaNShetty <namratanshetty@gmail.com>
2021-08-29 22:11:56 +0530

    Added: Messsage should return SAD

---
+++ src/com/bridgelabz/MoodAnalyserTest.java
@@ -0,0 +1,24 @@
+package com.bridgelabz;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MoodAnalyserTest
+{
+    MoodAnalyser moodAnalyser = null;
+
+    @Before
+    public void objectCreation()
+    {
+        moodAnalyser = new MoodAnalyser();
+    }
+
+    @Test
+    public void iAmIn_SadMood_Should_Return_sad()
+    {
+        String mood = moodAnalyser.analyseMood("This is sad message");
+        Assert.assertEquals("sad" , mood);
+        System.out.println(mood);
+    }
+}
